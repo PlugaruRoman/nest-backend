a54ccaf07ecc89f79085497a96feaa5e8d8003b9
Roman Plugaru <roman.plugaru@internal.ebs.md>
2026-02-02 15:36:04 +0200

    fix: fix prisma orm

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
+
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule)
+
+	const configService = app.get(ConfigService)
+
+	app.use(
+		cookieParser.default(configService.getOrThrow<string>('COOKIE_SECRET'))
+	)
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true
+		})
+	)
+
+	app.enableCors({
+		origin: configService.getOrThrow<string>('ALLOWED_ORIGINS'),
+		credentials: true,
+		exposedHeaders: ['Set-Cookie']
+	})
+
+	await app.listen(configService.getOrThrow<number>('APPLICATION_PORT'))
 }
-bootstrap();
+bootstrap()
